6ee27c9d8afbd30edbdaf609f562ef3c4d71fc2d
Erialbania <elope217@fiu.edu>
2020-08-31 12:52:05 -0400

    update appcontext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect } from './node_modules/react';
-import axios from './node_modules/axios'
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios'
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
