af34c2f1b0aa36453d233f2bc01941400cdbb98f
e-a-w- <eawatkins.0@gmail.com>
2020-09-01 12:15:09 -0400

    added mailto modal on profile page, moved payment button to profile, restored login code

--- client/src/Profiles/AllProfiles/ProfileButtons.jsx
+++ client/src/Profiles/AllProfiles/ProfileButtons.jsx
@@ -2,29 +2,31 @@ import React from 'react';
 import './profiles.css';
 import '../../colors.css';
 import Button from '@material-ui/core/Button';
-import {useHistory} from 'react-router-dom';
+import MailModal from '../UserProfile/MailModal';
+import { useHistory } from 'react-router-dom';
 
-const ProfileButton = ({role, ownerID}) => {
+const ProfileButton = ({ role, ownerID, userEmail }) => {
   const history = useHistory();
-  const leftButtonText = () => {
-    switch (role) {
-      case "owner":
-        return "Video Call";
-      case "sitter":
-        return "Tip Me!";
-      default:
-        return "Share";
-    }
-  }
 
   return (
     <div id="profile-btn-wrapper">
-      <Button variant="contained" id="btn1">
-        {leftButtonText()}
-      </Button>
-      {
-        ownerID ? <Button onClick={() => history.push(`/userprofile/${ownerID}`)} variant="contained" id="btn2">Owner</Button> : <Button variant="contained" id="btn2">Chat</Button>
-      }
+      {/* LEFT BUTTON */}
+      <MailModal role={role} userEmail={userEmail} />
+
+      {/* RIGHT BUTTON */}
+      {ownerID ? (
+        <Button
+          onClick={() => history.push(`/userprofile/${ownerID}`)}
+          variant="contained"
+          id="btn2"
+        >
+          Owner
+        </Button>
+      ) : (
+        <Button variant="contained" id="btn2">
+          Chat
+        </Button>
+      )}
     </div>
   );
 };
